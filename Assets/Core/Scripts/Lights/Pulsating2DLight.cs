e714bc788a1ad36631a477a32332a1f2346ef226
Althandir <Jacky.James.Quan@gmail.com>
2020-08-16 22:45:52 +0200

    Added Pulsating 2D Lights for Gate, Modified Light falloff for Window

---
+++ Assets/Core/Scripts/Lights/Pulsating2DLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+[RequireComponent(typeof(Light2D))]
+public class Pulsating2DLight : MonoBehaviour
+{
+    float _basevalue;
+    Light2D _light;
+
+    private void Awake()
+    {
+        _light = GetComponent<Light2D>();
+        _basevalue = _light.intensity;
+    }
+
+    private void FixedUpdate()
+    {
+        _light.intensity = _basevalue + Mathf.Sin(Time.time*3) / 2;
+    }
+}

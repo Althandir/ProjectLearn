17138dc2f1c6c48bc91f16e4fe33cc9239220e6a
Althandir <Jacky.James.Quan@gmail.com>
2020-10-01 23:59:41 +0200

    Created GameSettings
    - settings can be saved and loaded
    - currently no connection to Audio

---
+++ Assets/Core/Scripts/Core/MainMenu/AudioSliderLoader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{ 
+    [RequireComponent(typeof(Slider))]
+    public class AudioSliderLoader : MonoBehaviour
+    {
+        [SerializeField] AudioSetting _source;
+
+        void Start()
+        {
+            Slider slider = GetComponent<Slider>();
+            Core.GameSettings gameSettingsRef = Core.GameSettings.Instance;
+            switch (_source)
+            {
+                case AudioSetting.Master:
+                    slider.SetValueWithoutNotify(gameSettingsRef.GetMasterVolume());
+                    break;
+                case AudioSetting.Music:
+                    slider.SetValueWithoutNotify(gameSettingsRef.GetMusicVolume());
+                    break;
+                case AudioSetting.Effects:
+                    slider.SetValueWithoutNotify(gameSettingsRef.GetEffectsVolume());
+                    break;
+                default:
+                    Debug.LogWarning("AudioSliderLoader has no sourcevalue");
+                    break;
+            }
+        }
+    }
+
+    enum AudioSetting
+    {
+        Master, Music, Effects
+    }
+}
---
+++ Assets/Core/Scripts/Core/Settings/GameSettings.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Core
+{
+    [CreateAssetMenu(fileName = "Settings", menuName = "GameSettings", order = 0)]
+    public class GameSettings : ScriptableObject
+    {
+        public static GameSettings _instance;
+
+        [SerializeField] bool Debug_loadNow;
+        [SerializeField] bool Debug_clearSettingsFile;
+        [SerializeField] bool Debug_openDirectory;
+
+        [SerializeField] GameSettingsContainer _gameSettings = new GameSettingsContainer();
+
+        [SerializeField] string _fileName = "settings.ini";
+        string _pathToSettings;
+        public static GameSettings Instance { get => _instance; }
+
+        #region Unity Messages
+        private void OnEnable()
+        {
+            CreateSingleton();
+            CreatePathToSettingsIni();
+
+            LoadSettingsFromDisk();
+        }
+
+        /// <summary>
+        /// Debug Methods to be used inside the Unity Inspector.
+        /// </summary>
+        private void OnValidate()
+        {
+            if (Debug_loadNow)
+            {
+                Debug_loadNow = false;
+
+                LoadSettingsFromDisk();
+            }
+            if (Debug_clearSettingsFile)
+            {
+                Debug_clearSettingsFile = false;
+
+                if (File.Exists(_pathToSettings))
+                {
+                    File.Delete(_pathToSettings);
+                    Debug.Log("settings.ini deleted.");
+                }
+                else
+                {
+                    Debug.Log("No settings.ini found.");
+                }
+            }
+            if (Debug_openDirectory)
+            {
+                Debug_openDirectory = false;
+
+                Application.OpenURL(Application.persistentDataPath);
+            }
+
+        }
+        #endregion
+
+        void CreatePathToSettingsIni()
+        {
+            _pathToSettings = Path.Combine(Application.persistentDataPath, _fileName);
+        }
+
+        #region Singleton
+        void CreateSingleton()
+        {
+            if (!_instance)
+            {
+                _instance = this;
+                Debug.Log("Settings initialized.");
+            }
+            else
+            {
+                Debug.LogError("Duplicated Settings found!");
+            }
+        }
+        #endregion
+        
+        void LoadSettingsFromDisk()
+        {
+            if (File.Exists(_pathToSettings))
+            {
+                Debug.Log("Settings found.");
+                try
+                {
+                    JsonUtility.FromJsonOverwrite(File.ReadAllText(_pathToSettings), _gameSettings);
+                }
+                catch (Exception)
+                {
+                    Debug.LogError("Corrupted settingsfile detected! Recreating.");
+                    CreateNewSettingsFile();
+                    throw;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Settingsfile not found. Creating new File.");
+                CreateNewSettingsFile();
+            }
+        }
+
+        /// <summary>
+        /// Creates a new Settingsfile with a default GameSettingsContainer.
+        /// </summary>
+        void CreateNewSettingsFile()
+        {
+            try
+            {
+                SaveSettingsToDisk();
+            }
+            catch (Exception)
+            {
+                Debug.LogError("Could not create new Settingsfile!");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Writes the values in gameSettingsContainer into the settings.ini
+        /// </summary>
+        public void SaveSettingsToDisk()
+        {
+            File.WriteAllText(_pathToSettings, JsonUtility.ToJson(_gameSettings, true));
+            Debug.Log("Settings saved!");
+        }
+
+        #region Setter for GameSettingsContainer Values
+        public void SetMasterVolume(Single value)
+        {
+            _gameSettings.masterVolume = (float)Math.Round(value,2);
+        }
+
+        public void SetMusicVolume(Single value)
+        {
+            _gameSettings.musicVolume = (float)Math.Round(value, 2);
+        }
+
+        public void SetEffectsVolume(Single value)
+        {
+            _gameSettings.effectsVolume = (float)Math.Round(value, 2);
+        }
+        #endregion
+
+        #region Getter for GameSettingsContainer Values
+        public float GetMasterVolume()
+        {
+            return _gameSettings.masterVolume;
+        }
+
+        public float GetMusicVolume()
+        {
+            return _gameSettings.musicVolume;
+        }
+
+        public float GetEffectsVolume()
+        {
+            return _gameSettings.effectsVolume;
+        }
+        #endregion
+    }
+
+
+
+    [Serializable]
+    public class GameSettingsContainer
+    {
+        public float masterVolume;
+        public float musicVolume;
+        public float effectsVolume;
+
+        /// <summary>
+        /// Creates new Settings with each value set to 0.5f
+        /// </summary>
+        public GameSettingsContainer()
+        {
+            masterVolume = 0.5f;
+            musicVolume = 0.5f;
+            effectsVolume = 0.5f;
+        }
+
+        /// <summary>
+        /// Creates new Settings with the given parameters
+        /// </summary>
+        /// <param name="masterVolume"></param>
+        /// <param name="musicVolume"></param>
+        /// <param name="effectsVolume"></param>
+        public GameSettingsContainer(float masterVolume, float musicVolume, float effectsVolume)
+        {
+            this.masterVolume = masterVolume;
+            this.musicVolume = musicVolume;
+            this.effectsVolume = effectsVolume;
+        }
+    }
+}

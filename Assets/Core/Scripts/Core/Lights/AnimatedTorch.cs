9cf2769bb1d60d00f7cb1996fce99fa43202800b
Althandir <Jacky.James.Quan@gmail.com>
2020-10-27 20:22:33 +0100

    Added Audio from Jonas & created animated Torch

---
+++ Assets/Core/Scripts/Core/Lights/AnimatedTorch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AnimatedTorch : MonoBehaviour
+{
+    [SerializeField] float _delay = 1;
+
+    private void Start()
+    {
+        StartCoroutine(RotateTorch());
+    }
+
+    IEnumerator RotateTorch()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(_delay);
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    private void Reset()
+    {
+        _delay = 1;
+    }
+}
